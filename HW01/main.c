d194e7ea776d436679f4ea751fc1d0f824200b07
LinJ0 <n96104137@gs.ncku.edu.tw>
2022-06-30 14:40:25 +0800

    Upload main.c Application of function pointer / array of function pointer in C

--- HW01/main.c
+++ HW01/main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
+int (*fptr_array[])(int, int) = {plus, minus, multiply, divided};
+
+//string array oprand[] is for strcmp() to compare "string"
+char *operand[] = {"+", "-", "*", "/"};
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int size = sizeof(fptr_array) / (sizeof(int *)); //sizeof(int *) == 8 Bytes
+    
+    if(argc == 4){
+        for(i = 0; i < size; i++){
+	    if(strcmp(argv[2], operand[i]) == 0){
+	    //must transfer string (datatype of argv[]) to integer by atoi()
+            int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+	    printf("%d %s %d = %d\n", num1, argv[2], num2, fptr_array[i](num1, num2));
+	    }
+	}
+    }	
+}
+/*
 int main()
 {
 	int a, c;
@@ -21,3 +44,4 @@ int main()
 	else if (b == '/')
 		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
 }
+*/
