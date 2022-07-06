6d919996b96e8b36dc652c1158af33913d3a3319
LinJ0 <n96104137@gs.ncku.edu.tw>
2022-07-06 17:19:36 +0800

    Update main.s

--- HW02/main.s
+++ HW02/main.s
@@ -6,21 +6,21 @@
 .global _start
 .type _start, %function
 _start:
-	nop
-
-	//
-	//branch w/o link
-	//
-	b	label01
-
-label01:
-	nop
-
-	//
-	//branch w/ link
-	//
+	movs	r0, #0x1
+	movs	r1, #0x2
+	movs	r2, #0x3
+	movs	r3, #0x4
+push_reg:
+	push	{r0, r1, r2, r3}
+	push	{r3, r0, r2, r1}	//push together
+	push	{r3}			//push separate
+	push	{r0}
+	push	{r2}
+	push	{r1}
+pop_reg:
+	pop	{r0, r1, r2, r3}
+	pop	{r4, r5, r6, r7}
+	pop	{r8, r9, r10, r11}
 	bl	sleep
-
 sleep:
-	nop
 	b	.
