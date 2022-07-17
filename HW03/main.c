5c80c79121bb40970f2182fe7ae8f5bda4e34e4a
LinJ0 <n96104137@gs.ncku.edu.tw>
2022-07-17 13:33:15 +0800

    Update main.c Passing and return int, float and double.

--- HW03/main.c
+++ HW03/main.c
@@ -1,5 +1,30 @@
+#include <stdio.h>
+
+int int_1w1 = 111;
+int int_1w2 = 222;
+int int_1w3 = 333;
+int int_1w4 = 444;
+int int_1w5 = 555;
+float float_1w = 1111.2222;
+double double_2w = 3333.4444;
+
+
+int callee_int(int i1, int i2, int i3, int i4, int i5)
+{
+    return (i1 + i2 + i3 + i4 + i5);
+}
+float callee_float(float f){return f;}
+double callee_double(double d){return d;}
+
+
 void reset_handler(void)
 {
-	while (1)
-		;
+    int temp1;
+    float temp2;
+    double temp3;
+
+    temp1 = callee_int(int_1w1, int_1w2, int_1w3, int_1w4, int_1w5);
+    temp2 = callee_float(float_1w);
+    temp3 = callee_double(double_2w);
+    while (1);
 }
