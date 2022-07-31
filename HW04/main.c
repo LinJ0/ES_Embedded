a2033b4d6a4cf77f589c320cb2c37fb24102006d
LinJ0 <n96104137@gs.ncku.edu.tw>
2022-07-31 14:16:49 +0800

    Upload readme.md

--- HW04/main.c
+++ HW04/main.c
@@ -1,6 +1,14 @@
+#include <stdint.h>
 #include "blink.h"
+#include "button.h"
+#include "reg.h"
 
 int main(void)
 {
-	blink(LED_BLUE);
+    button_init(BUTTON_USER);
+    
+    while(!READ_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_IDR_OFFSET, IDRy_BIT(BUTTON_USER))){
+        blink_count(LED_ORANGE, 1);
+    }
+    blink(LED_BLUE);
 }
